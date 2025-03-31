33adfe72b5bb9ea225798d6bf62a77dbaebdaaa3
Matheus <matheusraimundosantos@gmail.com>
2025-03-31 10:50:15 -0300

    Adcionando arquivos do projeto

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let circle = document.querySelector('circle')
+let image = documento.querySelector('image')
+
+function changeColor(color) {
+  circle.style.background = color
+}
+
+function chageImage(img){
+  image.src = img
+}
